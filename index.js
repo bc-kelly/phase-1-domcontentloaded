a98db6de646018608cbcaf5fc6c41fd8214dfaea
Bridget Kelly <bckelly197@gmail.com>
2022-02-06 21:58:52 -0500

    commit tree 59e0524566cc53dc79db54879ecfe39bc2f8fc54

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    whenUpdated()
+});
+
+function whenUpdated() {
+document.getElementById('text').textContent = "This is really cool!";
+}
